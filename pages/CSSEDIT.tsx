b5050fe8643a970a15ba01482382c2c1b22f8dc4
Ambushfall <lukathetormentor@gmail.com>
2022-11-22 17:27:09 +0100

    Added style

--- pages/CSSEDIT.tsx
+++ pages/CSSEDIT.tsx
@@ -4,7 +4,7 @@ import '../app/globals.css'
 export default function CSSEDIT() {
     return (
         <div className={styles.container}>
-            <style id='STYLEDIV' contentEditable style={{display: 'inline'}}>{styles.main}
+            <style id='STYLEDIV' contentEditable style={{display: 'inline', position:"absolute", top:"60%", left:"43%", minWidth:"500px", minHeight:"500px"}}>{styles.main}
             </style>
             <main className={styles.main}>
                 <p>Hello</p>
