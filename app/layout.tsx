39927bd3a2143cb7278769b5e4e5c2ac82822188
Ambushfall <lukathetormentor@gmail.com>
2022-11-27 03:11:34 +0100

    Added Gradient background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import GradientBG from './gradientBackground'
 
 export default function RootLayout({
   children,
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <GradientBG/>
+        {children}
+        </body>
     </html>
   )
 }
--- app/marko/page.tsx
+++ app/marko/page.tsx
@@ -1,12 +1,13 @@
 
 import Image from 'next/image'
+// import GradientBG from './hero'
 
 
 export default function Page() {
     return (<>
         <ComponentOne />
         <ComponentTwo />
-        <ComponentThree />
+        {/* <ComponentThree /> */}
     </>)
 }
 
@@ -16,7 +17,7 @@ const ComponentOne = () => {
         <>
             <div className='py-0 p-8'>
                 <main className='min-h-screen flex flex-col justify-center items-center p-16 flex-1'>
-                    <Image alt='Test' src={'/frame5.svg'}  width={150} height={150} />
+                    <Image alt='Test' src={'/frame5.svg'} width={150} height={150} />
                 </main>
             </div>
         </>
@@ -25,13 +26,17 @@ const ComponentOne = () => {
 
 const ComponentTwo = () => {
     return (
-        <></>
+        <>
+        {/* <GradientBG/> */}
+        </>
     )
 }
 
 const ComponentThree = () => {
     return (
-        <></>
+        <>
+        <Image alt='Test' src={'/frame5.svg'} width={150} height={150} />
+        </>
     )
 }
 
