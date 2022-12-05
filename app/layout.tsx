2056d81bbad3124abb5e756f15192362f22c67d6
Ambushfall <lukathetormentor@gmail.com>
2022-12-05 03:11:11 +0100

    Added changes for App dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,25 @@
 import './globals.css'
 import GradientBG from './gradientBackground'
 import Navigation from './navigation'
+import { promises as fs } from 'fs'
+import path from 'path'
 
 
 
-export default function RootLayout({
+// eslint-disable-next-line
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
 
+
+    const regEx = new RegExp(/^([^.]+)$/g)
+    const postsDirectory = path.join(process.cwd(), '/app')
+    const filenames = await fs.readdir(postsDirectory)
+    const filterDirectories = filenames.filter((e) => regEx.test(e) ? e : false)
+
+  
   return (
     <html lang="en">
       {/*
@@ -18,7 +28,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <Navigation/>
+        <Navigation urls={filterDirectories}/>
         <GradientBG/>
         {children}
         </body>
--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,24 +1,25 @@
-import Test, {getStaticProps} from './test'
+import { link } from "fs";
 import Link from "next/link";
 
 
-const Navigation = async () => {
+const Navigation = ({ urls }:{urls:any}) => {
+  console.log(urls)
 
-const linkStyle = `text-base font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out dark:text-white dark:hover:text-gray-200`
-const urls = await getStaticProps();
+  const linkStyle = `text-base font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out dark:text-white dark:hover:text-gray-200`
 
   return (
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6">
-          <div>
-            <Link href={'/'}  className="text-lg font-medium text-gray-900 dark:text-white">Home</Link>
-          </div>
-          <nav className="hidden md:flex space-x-8">
-            {/* <Link href='/leaveserver'  className={linkStyle}>asd</Link> */}
-            <Test {...urls.props}/>
-          </nav>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between items-center border-b-2 border-gray-100 py-6">
+        <div>
+          <Link href={'/'} className={linkStyle}>Home</Link>
         </div>
+        <nav className="hidden md:flex space-x-8">
+          <>
+            {urls.map((el:string) => <Link className={linkStyle} href={`/${el}`} key={el}>{el}</Link>)}
+          </>
+        </nav>
       </div>
+    </div>
   )
 }
 
