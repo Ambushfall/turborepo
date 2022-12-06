a27be242b6e300822188ffe5e2a4a6096d0a10fc
Ambushfall <lukathetormentor@gmail.com>
2022-12-07 00:39:53 +0100

    ddf

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,25 +1,25 @@
 import Todo from './todo';
 
-interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
-}
+// interface Todo {
+//     id: number;
+//     title: string;
+//     completed: boolean;
+// }
 
 async function getTodos() {
-    console.log(process.env.NEXT_PUBLIC_HOST)
+    // console.log(process.env.NEXT_PUBLIC_HOST)
     const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/todos`);
-    // const todos: Todo[] = await res.json();
-    // return todos;
+    const todos = await res.json();
+    return todos;
 }
 
 export default async function Page() {
-    // const todos = await getTodos();
+    const todos = await getTodos();
     return (
         <ul>
-            {/* {todos.map((todo) => (
+            {todos.map((todo:any) => (
                 <Todo key={todo.id} {...todo} />
-            ))} */}
+            ))}
             {process.env.NEXT_PUBLIC_HOST}
         </ul>
     );
