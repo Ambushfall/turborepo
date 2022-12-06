85530608a0ab3ff98af00211751513b711fe96df
Ambushfall <lukathetormentor@gmail.com>
2022-12-07 00:34:48 +0100

    test

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -20,6 +20,7 @@ export default async function Page() {
             {/* {todos.map((todo) => (
                 <Todo key={todo.id} {...todo} />
             ))} */}
+            {process.env.NEXT_PUBLIC_HOST}
         </ul>
     );
 }
