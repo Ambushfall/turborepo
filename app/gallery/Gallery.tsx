b436535d695e9e9f8adeca1ea9b0bee7936853d4
Ambushfall <lukathetormentor@gmail.com>
2022-12-13 17:17:10 +0100

    Changes, error handling

--- app/gallery/Gallery.tsx
+++ app/gallery/Gallery.tsx
@@ -1,12 +1,13 @@
 'use client'
 import Image from 'next/image';
-import styles from '../page.module.css'
+import styles from '../page.module.css';
+const { grid, card } = styles;
 
-const { grid, card } = styles
 
 export const Gallery = ({ images }: {
     images: Array<string>;
 }) => {
+
     return <div className={grid}>
         {images.map((el: string) => <Image onClick={(e) => console.log(e)} className={card} width={150} height={150} alt={'alt'} src={`/midj/${el}`} key={el} />)}
     </div>;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image';
 import { Octokit } from "@octokit/core";
 import styles from '../page.module.css'
 const { container, main, title, grid, card } = styles
 
 const GitHubUserName = 'Ambushfall';
-const maxPages = 2;
 const hideForks = true;
 const octokit = new Octokit({ auth: process.env.PRS_ACC_TOK });
 
+
 const linkContainerDivStyle = 'inline-block align-middle justify-center items-center'
+const descriptionClassName = "line-clamp-1 text-sm m-2 text-gray-600"
 
 // display infomation from github profile
 /* const displayProfile = ({ profile }: { profile: any }) => {
@@ -34,30 +34,16 @@ const linkContainerDivStyle = 'inline-block align-middle justify-center items-ce
 
 // get list of user's public repos
 const getRepos = async () => {
-    let repos: any[] = [];
-    for (let i = 1; i <= maxPages; i++) {
-        const request = await octokit.request('GET /users/{username}/repos{?type,sort,direction,per_page,page}', {
-            username: GitHubUserName,
-            sort: 'pushed',
-            per_page: 100,
-            page: { i },
-            type: 'public'
-        })
-        // console.log(request.data)
-        repos = repos.concat(request.data);
-    }
-    repos.sort((a, b) => b.forks_count - a.forks_count);
-    repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    const request = await octokit.request('GET /users/{username}/repos{?type,sort,direction,per_page,page}', {
+        username: GitHubUserName,
+        sort: 'pushed',
+        per_page: 100,
+        type: 'all'
+    })
 
-    var filterArray = repos.reduce((accumulator, current) => {
-        if (!accumulator.some((item: any) => item.id === current.id && item.name === current.name)
-        ) {
-            accumulator.push(current);
-        }
-        return accumulator;
-    }, []);
+    const { data, headers, url, status } = request
 
-    return filterArray
+    return status === 200 ? data : status
 };
 
 
@@ -65,22 +51,22 @@ const getRepos = async () => {
 // display list of all user's public repos
 const DisplayRepos = ({ repos }: { repos: any[] }) => {
     const userHome = `https://github.com/${GitHubUserName}`
-    // repos.forEach(v => console.log(v.name))
 
-    return <>{repos.map((v, k) => v.fork ? false :
-        <li className='inline-block m-4 p-6 border-[#eaeaea] dark:border-[#222] text-center align-middle bg-inherit border-2 rounded-xl' key={k} >
+
+    return <>{repos.map((v, k) => hideForks && v.fork ? false :
+        <li className='inline-block max-w-xs max-h-min m-4 p-6 border-[#eaeaea] dark:border-[#222] text-center align-middle bg-inherit border-2 rounded-xl' key={k} >
 
             <p className='text-xl font-medium' >{v.name}</p>
-            <span className="line-clamp-1 text-sm m-2 text-gray-500" >{v.description}</span>
+            {v.description ? <span className={descriptionClassName}>{v.description}</span> : <p className={descriptionClassName}>No description</p>}
 
             {v.stargazers_count > 0 && <a className='relative col-span-1 float-right' href={`${userHome}/${v.name}/stargazers`}>
                 <span className='relative col-span-1' >⭐ {v.stargazers_count}</span></a>}
-            {v.language !== null ? <a href={`${userHome}?tab=repositories&q=&language=${v.language}`}></a> : <p ></p>}
+            {v.language !== null ? <a href={`${userHome}?tab=repositories&q=&language=${v.language}`}></a> : false}
             {v.forks_count > 0 && <a href={`${userHome}/${v.name}/network/members`}>
                 <span >{v.forks_count}</span></a>}
             {v.homepage && v.homepage !== "" ? <div className={linkContainerDivStyle}>
                 <a className='relative col-span-2' href={v.html_url}>Code </a>
-                <a className='relative col-span-3' href={v.homepage}>Live </a> </div> : <div className={linkContainerDivStyle}>
+                <a className='relative col-span-3' href={v.homepage} >Live </a> </div> : <div className={linkContainerDivStyle}>
                 <a className='relative col-span-4' href={v.html_url}>View Project</a></div>}
         </li>)}
     </>
@@ -89,6 +75,13 @@ const DisplayRepos = ({ repos }: { repos: any[] }) => {
 
 export default async function Page() {
     const repos = await getRepos()
+    if (typeof repos === 'number') return (
+        <div className={container} >
+            <main className={main}>
+                <h1 className={title}>Error: {repos}</h1>
+            </main>
+        </div>
+    )
     // const request = await octokit.request('GET /users/{username}', { username: GitHubUserName })
 
     return (
