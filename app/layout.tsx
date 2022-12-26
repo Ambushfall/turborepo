25267dec9ade5a478dc2365b5cd49f1b1897c5e7
Ambushfall <lukathetormentor@gmail.com>
2022-12-26 21:40:24 +0100

    Added DaisyUI, Replaced Navbar with Drawer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,20 @@ import './globals.css'
 import GradientBG from './components/gradientBackground'
 import { promises as fs } from 'fs'
 import path from 'path'
-import NavBarTailwind from './components/NavBarTailwind'
 import { AnalyticsWrapper } from './components/analytics';
+import { NavLinkTailwind } from './components/NavLinkTailwind';
+
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  
+
   const reg = /^([^.]+)$/g;
   const appDirectory = path.join(process.cwd(), '/app');
   const appFilenames = await fs.readdir(appDirectory)
   const filterDirectories = appFilenames.filter((e) => e.match(reg) ? e !== 'components' : false)
-
   return (
     <html lang="en">
       {/*
@@ -24,10 +24,36 @@ export default async function RootLayout({
       */}
       <head />
       <body >
-        <NavBarTailwind urls={filterDirectories} />
-        <GradientBG />
-        {children}
-        <AnalyticsWrapper />
+
+        <div className="drawer">
+          <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col">
+            <div className="w-full navbar bg-transparent">
+              <div className="flex-none lg:hidden">
+                <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </label>
+              </div>
+              <div className="flex-1 px-2 mx-2"></div>
+              <div className="flex-none hidden lg:block">
+                <ul className="menu menu-horizontal">
+                  <NavLinkTailwind href='/' NavItemName='Home' />
+                  {filterDirectories?.map((el: string) => <NavLinkTailwind key={`horizontal${el}`} href={`/${el}`} NavItemName={el} />)}
+                </ul>
+              </div>
+            </div>
+            <GradientBG />
+            {children}
+            <AnalyticsWrapper />
+          </div>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+            <ul className="menu p-4 w-80 bg-base-100">
+              <NavLinkTailwind href='/' NavItemName='Home' />
+              {filterDirectories?.map((el: string) => <NavLinkTailwind key={`sidebar${el}`} href={`/${el}`} NavItemName={el} />)}
+            </ul>
+          </div>
+        </div>
       </body>
     </html>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
+    require("daisyui"),
   ],
+
 }
