b103a1db12216389768ffa303afef4f5bb7d8e0e
Ambushfall <lukathetormentor@gmail.com>
2022-12-26 21:49:39 +0100

    Added toPascalCase

--- app/components/NavLinkTailwind.tsx
+++ app/components/NavLinkTailwind.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
+import {toPascalCase} from '../../helpers/toPascalCase'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -14,7 +15,7 @@ export function NavLinkTailwind({ href, NavItemName }: { href: string; NavItemNa
 
     return (
         <Link href={href} className={classNames(isActive? 'bg-gray-900 text-white dark:bg-gray-400 dark:text-black' : 'dark:text-gray-400 dark:hover:bg-gray-800 text-black hover:bg-gray-700 hover:text-white','px-3 py-2 rounded-md text-2xl font-medium')} aria-current={isActive ? 'page' : undefined}>
-            {NavItemName}
+            {toPascalCase(NavItemName)}
         </Link>
     );
 }
