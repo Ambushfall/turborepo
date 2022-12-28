2eb0965fc29012a1897e136dcd6e18628fc4d6ff
Ambushfall <lukathetormentor@gmail.com>
2022-12-28 14:49:35 +0100

    Style Issues with Project page

--- app/projects/Constants.tsx
+++ app/projects/Constants.tsx
@@ -1,4 +1,4 @@
 export const GitHubUserName = 'Ambushfall';
 export const hideForks = true;
-export const linkContainerDivStyle = 'inline-block align-middle justify-center items-center';
+export const linkContainerDivStyle = 'inline-block align-middle justify-center items-center text-center';
 export const descriptionClassName = 'line-clamp-1 text-sm m-2 text-gray-600';
