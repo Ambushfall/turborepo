7373160f5d1717922c4c02c2bf2d8138f2d129d5
Ambushfall <lukathetormentor@gmail.com>
2022-12-28 14:49:42 +0100

    Style Issues with Project page

--- app/projects/RepositoryModule.tsx
+++ app/projects/RepositoryModule.tsx
@@ -7,7 +7,7 @@ export const RepositoryModule = ({ repoData  }: { repoData: any[]; }) => {
 
 
     return <>{repoData.map((v, k) => hideForks && v.fork ? false :
-        <li className='inline-block max-w-xs max-h-min m-4 p-6 border-[#eaeaea] dark:border-[#222] text-center align-middle bg-inherit border-2 rounded-xl' key={k}>
+        <li className='inline-block max-w-xs max-h-min m-4 p-6 border-[#eaeaea] dark:border-[#222] text-center align-middle bg-inherit border-2 rounded-xl hover:border-slate-200 hover:shadow-lg hover:shadow-slate-200' key={k}>
 
             <p className='text-xl font-medium'>{v.name}</p>
             {v.description ? <span className={descriptionClassName}>{v.description}</span> : <p className={descriptionClassName}>No description</p>}
@@ -18,9 +18,9 @@ export const RepositoryModule = ({ repoData  }: { repoData: any[]; }) => {
             {v.forks_count > 0 && <a href={`${userHome}/${v.name}/network/members`}>
                 <span>{v.forks_count}</span></a>}
             {v.homepage && v.homepage !== '' ? <div className={linkContainerDivStyle}>
-                <a className='relative col-span-2' href={v.html_url}>Code </a>
-                <a className='relative col-span-3' href={v.homepage}>Live </a> </div> : <div className={linkContainerDivStyle}>
-                <a className='relative col-span-4' href={v.html_url}>View Project</a></div>}
+                <a className='m-1 relative col-span-2 border-[#eaeaea] dark:border-[#222] border-2 rounded-lg p-2 hover:bg-slate-500' href={v.html_url}>Code </a>
+                <a className='m-1 relative col-span-3 border-[#eaeaea] dark:border-[#222] border-2 rounded-lg p-2 hover:bg-slate-500' href={v.homepage}>Live </a> </div> : <div className={linkContainerDivStyle}>
+                <a className='m-1 relative col-span-4 border-[#eaeaea] dark:border-[#222] border-2 rounded-lg p-2 hover:bg-slate-500' href={v.html_url}>View Project</a></div>}
         </li>)}
     </>;
 };
