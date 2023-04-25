b35318df86ead6e7824ca0208ecc845aeebc26d5
Ambushfall <lukathetormentor@gmail.com>
2023-04-25 12:40:22 +0200

    removed link

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout({
   const reg = /^([^.]+)$/g;
   const appDirectory = path.join(process.cwd(), '/app');
   const appFilenames = await fs.readdir(appDirectory)
-  const filterDirectories = appFilenames.filter((e) => e.match(reg) ? e !== 'components' && e !== 'api' : false)
+  const filterDirectories = appFilenames.filter((e) => e.match(reg) ? e !== 'components' && e !== 'api' && e !== 'github' : false)
   return (
     <html lang="en">
       {/*
