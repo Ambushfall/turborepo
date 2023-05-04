3ddbac957abc79bb51cfa2334281e5ede4319274
Ambushfall <lukathetormentor@gmail.com>
2023-05-04 02:54:04 +0200

    Rem API Call

--- pages/api/predictions/index.ts
+++ pages/api/predictions/index.ts
@@ -13,14 +13,6 @@ export default async function handler(req, res) {
     );
   }
 
-  const output = await replicate.run(
-    "prompthero/openjourney:9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb",
-    {
-      input: {
-        prompt: `mdjrny-v4 style ${req.body.prompt}, volumetric lighting, octane render, 4 k resolution, trending on artstation, masterpiece`
-      }
-    })
-
   const prediction = await replicate.predictions.create({
     // Pinned to a specific version of Stable Diffusion
     // See https://replicate.com/stability-ai/stable-diffusion/versions
