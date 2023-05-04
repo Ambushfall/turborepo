d4a3609926d6aa30eccaadf32be55fc02d354c6c
Ambushfall <lukathetormentor@gmail.com>
2023-05-04 23:19:21 +0200

    Revert

--- app/components/NavLinkTailwind.tsx
+++ app/components/NavLinkTailwind.tsx
@@ -12,7 +12,7 @@ export function NavLinkTailwind({ href, NavItemName }: { href: string; NavItemNa
     const segment = useSelectedLayoutSegments();
 
     const isActive = href === `/${segment}`
-    console.log(href)
+    // console.log(href)
 
     return (
         <Link href={href} className={classNames(isActive? 'bg-gray-900 text-white dark:bg-gray-400 dark:text-black' : 'dark:text-gray-400 dark:hover:bg-gray-800 text-black hover:bg-gray-700 hover:text-white','px-3 py-2 rounded-md text-2xl font-medium')} aria-current={isActive ? 'page' : undefined}>
