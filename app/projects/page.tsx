b680ac7cfadaf6d4cef34a5126d5854399c4d384
Ambushfall <lukathetormentor@gmail.com>
2023-05-04 22:52:21 +0200

    Rem Tailwind-clamp + move to projects async

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,49 @@
 
 import styles from '@app/page.module.css';
 import ProjectModule from "./ProjectModule";
-import { getRepos } from '@utils/getrepos';
+// import { getRepos } from '@utils/getrepos';
 const { container, main, title, grid, card, description } = styles;
 const GitHubUserName = 'Ambushfall';
 
 // export const revalidate = 60;
-
+const getRepos = async (userName: string) => {
+    // .........
+    const route = `https://api.github.com/users/${userName}/repos?&sort=pushed&per_page=100`
+    if (process.env.PRS_ACC_TOK) {
+      const parameters = {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization:
+            process.env.PRS_ACC_TOK
+        },
+        next: {
+          revalidate: 60*24
+        }
+      }
+      try {
+        
+        const res = await fetch(route, parameters)
+        const data : any[] = await res.json();
+        data.sort((a, b) => b.forks_count - a.forks_count);
+        data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+        return data
+      } catch (error: any) {
+        if (error?.response && error.status === 403 && error.response.headers['x-ratelimit-remaining'] === '0') {
+          const resetTimeEpochSeconds = error.response.headers['x-ratelimit-reset'];
+          const currentTimeEpochSeconds = Math.floor(Date.now() / 1000);
+          const secondsToWait = resetTimeEpochSeconds - currentTimeEpochSeconds;
+          console.log(`You have exceeded your rate limit. Retrying in ${secondsToWait} seconds.`);
+          setTimeout(fetch, secondsToWait * 1000, route, parameters);
+        }
+  
+        if (error?.data?.message == 'Bad credentials') {
+          return error.data.message
+        }
+      }
+    } else {
+      return 404
+    }
+  }
 
 export default async function Page() {
     const repos = await getRepos(GitHubUserName)
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
     require("daisyui")
   ],
