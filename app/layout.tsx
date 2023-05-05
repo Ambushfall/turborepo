7694addaeb71e7371e8aa7d636e0850aa00f310e
Ambushfall <lukathetormentor@gmail.com>
2023-05-06 00:23:30 +0200

    Test23

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default async function RootLayout({
     <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        head.tsx.  Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
       <body >
