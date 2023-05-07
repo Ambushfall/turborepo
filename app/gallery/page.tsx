e240fcfc3b2d7e5ba1f3f3426854be723b90eb4e
Ambushfall <lukathetormentor@gmail.com>
2023-05-07 23:10:00 +0200

    Test5

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,10 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+
 import styles from '@app/page.module.css'
 import { Gallery } from '@components/Gallery';
 import { twtitle } from '@tw/styles';
-import { getImages } from '@utils/getImages';
+
 
 const { container, main, title } = styles
 
@@ -10,14 +13,17 @@ export const revalidate = 10;
 
 export default async function Page() {
 
-    const data = await getImages()
+    const imageDirectory = path.join(process.cwd(), '/public/midj');
+
+    /* Reads the content of the midj dir and returns an array of strings */
+    const imageFilenames = await fs.readdir(imageDirectory)
 
     return (
         <>
             <div className={container}>
                 <div className={main}>
                     <Title Name='Gallery' Title={'Proof of Concept'} />
-                    <Gallery images={data} />
+                    <Gallery images={imageFilenames} />
                 </div>
             </div>
         </>
