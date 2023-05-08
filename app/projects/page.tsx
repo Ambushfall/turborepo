c6253199f4f78e5de19df08074aaf9219d99f1a9
Ambushfall <lukathetormentor@gmail.com>
2023-05-08 04:40:24 +0200

    add Turbo, test reval once again, node :<

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import 'server-only'
 
 import styles from '@app/page.module.css';
 import { twtitle } from '@tw/styles';
-import ProjectModule from "@components/ProjectModule";
+import ProjectModule from "./ProjectModule";
 const { container, main, description, title } = styles;
 const GitHubUserName = 'Ambushfall';
 
@@ -10,34 +10,35 @@ const GitHubUserName = 'Ambushfall';
 
 async function fetchRepos(username: string) {
     // console.warn(process.env.NEXT_PUBLIC_HOST)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/github/${username}`, { cache: 'no-store' });
-    // Recommendation: handle errors
-    // if (!res.ok) {
-    //     // This will activate the closest `error.js` Error Boundary
-    //     throw new Error('Failed to fetch data');
-    // }
-    
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/github/${username}`,
+        {
+            next: {
+                revalidate: 60
+            }
+        });
     return res.json();
 }
 
 export default async function Page() {
     console.log('hello I AM SERVER COMPONENT WTF')
     const repos = await fetchRepos(GitHubUserName);
-    if (typeof repos === 'number' || typeof repos.message === 'string')
-
+    if (typeof repos.message === 'string') {
+        const { message, status, wait } = repos
         return (
             <div className={container} >
                 <main className={main}>
-                    <h1 className={title}>Error: {repos?.message ? repos.message : repos}</h1>
+                    <h1 className={title}>Code: {status}</h1>
+                    <h1 className={title}>{message}</h1>
                 </main>
             </div>
         )
+    }
 
     return (
         <>
             <div className={container} >
                 <main className={main}>
-                    <h1 className={title} >All Of My Projects</h1>
+                    <h1 className={twtitle} >All Of My Projects</h1>
                     <small className={description} >Some useful, some stupid, all fun!</small>
                     <section>
                         <div></div>
--- utils/getrepos.ts
+++ utils/getrepos.ts
@@ -1,10 +1,10 @@
-// import 'server-only'
+import 'server-only'
 export const getRepos = async (userName: string) => {
   const route = `https://api.github.com/users/${userName}/repos?&sort=pushed&per_page=100`
   if (process.env.PRS_ACC_TOK) {
     const parameters: RequestInit = {
       // cache: 'no-store',
-      next:{
+      next: {
         revalidate: 200
       },
       headers: {
@@ -15,26 +15,29 @@ export const getRepos = async (userName: string) => {
     }
     try {
       const res: Response = await fetch(route, parameters)
-      // console.log(res.headers.get('x-ratelimit-remaining'))
-      console.log(`I AM RESPONSE ${res}`)
+      console.log(res.headers.get('x-ratelimit-remaining'))
       if (res.status === 403 && res.headers.get('x-ratelimit-remaining') === '0') {
         const resetTimeEpochSeconds = Number(res.headers.get('x-ratelimit-reset'));
         const currentTimeEpochSeconds = Math.floor(Date.now() / 1000);
         const secondsToWait = resetTimeEpochSeconds - currentTimeEpochSeconds;
         console.log(`You have exceeded your rate limit. Retrying in ${secondsToWait} seconds.`);
         setTimeout(fetch, secondsToWait * 1000, route, parameters);
-        return res.status
+
+        return {
+          status: res.status,
+          remaining: res.headers.get('x-ratelimit-remaining'),
+          message: `You have exceeded your rate limit. Retrying in ${secondsToWait} seconds.`
+        }
       }
+      
       const data: any[] = await res.json();
-
-
       data.sort((a, b) => b.forks_count - a.forks_count);
       data.sort((a, b) => b.stargazers_count - a.stargazers_count);
 
       return data
     } catch (error: any) {
 
-      console.log(`getrepos.ts:26 Err:${error}`)
+      console.log(`getrepos.ts:40 Err:${error}`)
 
     }
   } else {
