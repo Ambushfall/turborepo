a20b0d6f09bc2b04a18528109ce322716eb00f6d
Ambushfall <lukathetormentor@gmail.com>
2023-05-08 02:18:13 +0200

    ErrHandling and test dynamic route updates

--- utils/getrepos.ts
+++ utils/getrepos.ts
@@ -1,6 +1,8 @@
 
+
+
+
 export const getRepos = async (userName: string) => {
-  // .........
   const route = `https://api.github.com/users/${userName}/repos?&sort=pushed&per_page=100`
   if (process.env.PRS_ACC_TOK) {
     const parameters = {
@@ -9,26 +11,30 @@ export const getRepos = async (userName: string) => {
         Authorization:
           process.env.PRS_ACC_TOK
       },
-      next: { revalidate: 10 }
+      next: { revalidate: 100 }
     }
     try {
       const res = await fetch(route, parameters)
-      const data: any[] = await res.json();
-      data.sort((a, b) => b.forks_count - a.forks_count);
-      data.sort((a, b) => b.stargazers_count - a.stargazers_count);
-      return data
-    } catch (error: any) {
-      if (error?.response && error.status === 403 && error.response.headers['x-ratelimit-remaining'] === '0') {
-        const resetTimeEpochSeconds = error.response.headers['x-ratelimit-reset'];
+      console.log(res.headers.get('x-ratelimit-remaining'))
+      if (res.status === 403 && res.headers.get('x-ratelimit-remaining') === '0') {
+        const resetTimeEpochSeconds = Number(res.headers.get('x-ratelimit-reset'));
         const currentTimeEpochSeconds = Math.floor(Date.now() / 1000);
         const secondsToWait = resetTimeEpochSeconds - currentTimeEpochSeconds;
         console.log(`You have exceeded your rate limit. Retrying in ${secondsToWait} seconds.`);
         setTimeout(fetch, secondsToWait * 1000, route, parameters);
+        return res.status
       }
+      const data: any[] = await res.json();
 
-      if (error?.data?.message == 'Bad credentials') {
-        return error.data.message
-      }
+
+      data.sort((a, b) => b.forks_count - a.forks_count);
+      data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+      return data
+    } catch (error: any) {
+
+      console.log(`getrepos.ts:26 Err:${error}`)
+
     }
   } else {
     return 404
