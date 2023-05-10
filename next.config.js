4b43148176643db3174a994fc7d6597b77a26b5f
Ambushfall <lukathetormentor@gmail.com>
2023-05-10 12:23:59 +0200

    Test CSP

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self';
+`;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -13,6 +21,20 @@ const nextConfig = {
     ],
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          }
+        ],
+      },
+    ];
+  },
+    
 }
 
 module.exports = nextConfig
