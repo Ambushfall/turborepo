23f0b614f273512c274431d2003230d3833eee60
Ambushfall <lukathetormentor@gmail.com>
2023-05-10 12:28:11 +0200

    Test2CS

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/about',
+        source: '/*',
         headers: [
           {
             key: 'Content-Security-Policy',
