39a96201acf82429775aa2417a663860b6cb20db
Ambushfall <lukathetormentor@gmail.com>
2024-07-04 03:02:31 +0200

    final

--- apps/experimentalapp/app/(vencanje)/vencanje/page.tsx
+++ apps/experimentalapp/app/(vencanje)/vencanje/page.tsx
@@ -7,8 +7,8 @@ export default function Page () {
     <div className={'px-8 py-0'}>
       <section className='min-h-screen flex-1 flex flex-col justify-center items-center  px-0 py-16 fadeIn'>
         <div className='backgroundElement z-0'></div>
-        <h1 className='naslov z-10 text-7xl text-[#30291b] capitalize font-extrabold italic'>suncica i luka</h1>
-        <p className='naslov z-10 pt-5'>Vas pozivaju da prisustvujete nasem vencanju<br/></p>
+        <h1 className='naslov z-10 text-7xl text-[#30291b] capitalize font-extrabold italic'>sunčica i luka</h1>
+        <p className='naslov z-10 pt-5'>Vas pozivaju da prisustvujete našem venčanju<br/></p>
         <h1 className='naslov uppercase z-10 text-[#c59536]'>14:30 Subota, 6. Jul, 2024</h1>
         <p className='naslov z-10 pt-5'>Opština Čukarica<br/>Šumadijski trg 2<br/>Banovo Brdo, Beograd</p>
         <a
