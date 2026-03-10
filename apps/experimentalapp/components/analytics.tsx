34283072208a471d0ea5289cffff1faa3322b377
Ambushfall <lukathetormentor@gmail.com>
2026-03-10 20:17:30 +0100

    Add gtm for testing

--- apps/experimentalapp/components/analytics.tsx
+++ apps/experimentalapp/components/analytics.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Suspense } from 'react'
 import { Analytics } from '@vercel/analytics/react'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export function AnalyticsWrapper () {
@@ -10,6 +10,7 @@ export function AnalyticsWrapper () {
       <Suspense>
         <Analytics />
         <SpeedInsights/>
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!}/>
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!} />
       </Suspense>
     </>
