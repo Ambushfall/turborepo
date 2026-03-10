bd5a67dba4ae6d035578ca26790a350de921e340
Ambushfall <lukathetormentor@gmail.com>
2026-03-10 13:49:24 +0100

    added speed insights

--- apps/experimentalapp/components/analytics.tsx
+++ apps/experimentalapp/components/analytics.tsx
@@ -2,12 +2,14 @@
 import { Suspense } from 'react'
 import { Analytics } from '@vercel/analytics/react'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export function AnalyticsWrapper () {
   return (
     <>
       <Suspense>
         <Analytics />
+        <SpeedInsights/>
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!} />
       </Suspense>
     </>
