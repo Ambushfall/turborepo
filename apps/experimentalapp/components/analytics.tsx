8a5719ff30c402420219cf308cd202e1e3538251
Ambushfall <lukathetormentor@gmail.com>
2026-03-10 13:32:51 +0100

    Added GA for testing

--- apps/experimentalapp/components/analytics.tsx
+++ apps/experimentalapp/components/analytics.tsx
@@ -1,6 +1,15 @@
-'use client';
-import { Analytics } from '@vercel/analytics/react';
+'use client'
+import { Suspense } from 'react'
+import { Analytics } from '@vercel/analytics/react'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-export function AnalyticsWrapper() {
-  return <Analytics />;
+export function AnalyticsWrapper () {
+  return (
+    <>
+      <Suspense>
+        <Analytics />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!} />
+      </Suspense>
+    </>
+  )
 }
